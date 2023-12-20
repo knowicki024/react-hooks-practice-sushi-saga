e56d94af49f9251e1104ecf980bcccf5f376f702
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-12-19 20:41:12 -0500

    build and burn

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const budget = 100;
+
+  const [sushis, setSushis] = useState([]);
+  const [money, setMoney] = useState(budget);
+
+useEffect(() => {
+  fetch(API)
+  .then(res => res.json())
+  .then(setSushis);
+}, []);
+
+function eatSushi(sushi) {
+  if (!sushi.eaten && sushi.price <= money) {
+  setMoney(money - sushi.price);
+  setSushis(sushis.map(sush => sush.id !== sushi.id ? sush :
+    {...sush, eaten: true}
+    )) 
+  }
+}
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} eatSushi={eatSushi}/>
+      <Table plates={sushis.filter(sushi => sushi.eaten)} money ={money}/>
     </div>
   );
 }
